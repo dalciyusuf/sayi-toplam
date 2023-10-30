58ef76a38dd4173e08951c9fa1af6d963dafc2f4
Yusuf <115079732+dalciyusuf@users.noreply.github.com>
2023-10-30 17:47:39 +0300

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+main()
+{
+	int i,n,sonuc;
+	
+	// girilen sayýya kadar olan sayýlarýn toplamýný veren algoritma 
+	printf("bir sayi giriniz:	");
+	scanf("%d",&n);
+	sonuc=0;
+	for (i=1;i<=n;i++)
+	{
+
+	sonuc = sonuc + i;
+	}	
+	printf("girdiginiz sayilarin toplami:	%d",sonuc);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0 ;
+}
